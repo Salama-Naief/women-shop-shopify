cea154291676db9fff8b7eefa59badc0b63ed16f
Salama-Naief <eng.salama.naief@gmail.com>
2022-11-03 17:05:59 +0200

    dds

--- utils/url.js
+++ utils/url.js
@@ -1,3 +1,3 @@
-export const API_URL=process.env.VERCEL_URL?`https://${process.env.VERCEL_URL}/api`:"http://localhost:3000/api"
+export const API_URL="https://women-shop-shopify-t21s.vercel.app/api"
 //"https://women-shop-shopify-l9nr.vercel.app"
 //process.env.VERCEL_URL?`https://${process.env.VERCEL_URL}/api`
